75ba77eb1c5e382542176c7c79141e6ee8c0941e
AnggaPutraa <yananggaa@gmail.com>
2023-05-03 20:13:59 +0700

    feat: navbar authorization

--- base/views.py
+++ base/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def main(request):
+    request.session['is_atlet'] = False
+    request.session['is_pelatih'] = False
+    request.session['is_umpire'] = True
     return render(request, 'index.html')
