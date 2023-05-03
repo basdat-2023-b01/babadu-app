d018d0064aaec1559b0110bdda3e337ee67ad286
AnggaPutraa <yananggaa@gmail.com>
2023-05-03 14:31:57 +0700

    style: lint

--- event/views.py
+++ event/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 
+
 def main(request):
-    return render(request, 'daftar_stadium.html')
+    return render(request, 'event_main.html')
+
 
 def daftar_event(request, id):
     return render(request, 'daftar_event.html')
 
-def daftar_kategori(request, id1,id2):
+
+def daftar_kategori(request, id1, id2):
     return render(request, 'daftar_kategori.html')
+
 
 def pertandingan(request, id):
     return render(request, 'pertandingan.html')
