1a6f1ea28eece998be0d1594a09e6eac3a22fcfa
AnggaPutraa <yananggaa@gmail.com>
2023-05-04 12:03:59 +0700

    refactor: dashboard for umpire

--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 def main(request):
     # temporary role authorization goes here
-    request.session['is_atlet'] = False
+    request.session['is_atlet'] = True
     request.session['is_pelatih'] = False
-    request.session['is_umpire'] = True
+    request.session['is_umpire'] = False
     return render(request, 'index.html')
