f6fc6071e88c66140aeaa7e4f4a3294cec9076f6
AnggaPutraa <yananggaa@gmail.com>
2023-05-05 09:52:56 +0700

    fix: scrollable and urls naming

--- tes_kualifikasi/urls.py
+++ tes_kualifikasi/urls.py
@@ -4,6 +4,6 @@ from tes_kualifikasi.views import *
 app_name = 'tes-kualifikasi'
 
 urlpatterns = [
-    path('data/kualifikasi', data_kualifikasi_view, name='data-kualifikasi'),
-    path('pertanyaan/kualifikasi', pertanyaan_kualifikasi_view, name='pertanyaan-kualifikasi'),
+    path('daftar-tes/', data_kualifikasi_view, name='data-kualifikasi'),
+    path('pertanyaan/', pertanyaan_kualifikasi_view, name='pertanyaan'),
 ]
